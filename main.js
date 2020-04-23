af536a3f2fb3a6a077ab61793de6f498f07116bb
Ficuligna <marcorandazzo051291@gmail.com>
2020-04-23 20:26:37 +0200

    Milestone 5 soluzione parziale trovata

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ $(document).ready(function () {
     $(".risultatiRicerca").html("");
     var valoreRicerca = $("input").val();
     cercaTitoli("https://api.themoviedb.org/3/search/movie",valoreRicerca,"film")
-    cercaTitoli("https://api.themoviedb.org/3/search/tv",valoreRicerca,"serie")
+    // cercaTitoli("https://api.themoviedb.org/3/search/tv",valoreRicerca,"serie")
   });
 
 // funzione ricerca - chiamata ajax
-  function cercaTitoli(url,parola,genere){   //inserire: url come stringa,per parola il risultato di ricerca, per genere se film o serie
+  function cercaTitoli(url,parola,tipo){   //inserire: url come stringa,per parola il risultato di ricerca, per tipo se film o serie
     $.ajax({
       url: url,
       data: {
@@ -20,27 +20,67 @@ $(document).ready(function () {
       },
       method: "GET",
       success: function(data,stato) {
+        var listaAttori = [];
         var risultati = data.results;
-        if (genere == "serie") {
+        if (tipo == "serie") {
           var titoloOriginale = "original_name";
           var titolo = "name";
-        }else if(genre = "film") {
+        }else if(tipo == "film") {
           var titoloOriginale = "original_title";
           var titolo = "title";
         }
         for (var i = 0; i < risultati.length; i++) {
+            var urlAttori;
+            if (tipo == "serie") {
+              urlAttori = 'https://api.themoviedb.org/3/tv/'+ risultati[i].id +'/credits'
+            }else if (tipo == "film") {
+              urlAttori = 'https://api.themoviedb.org/3/movie/'+ risultati[i].id +'/credits'
+            }
+            //la chiamata ajax non mantiene il dato [i]. Ma ad ogni [i] viene semplicemente richiamata. Quindi
+            //si può creare un ciclo per ogni chiamata ajax che associ questa al corrispondente risultati[i]
+            // $.ajax({
+            //
+            //   url: urlAttori,
+            //   data: {
+            //     api_key: "337a5096c47a89ed7fe0ca372a05c5bc",
+            //   },
+            //   method: "GET",
+            //   success: function (data,stato) {
+            //     // console.log(i);
+            //     var cast = data.cast;
+            //     var attori="";
+            //     for (var l = 0; l < 5; l++) {
+            //       if (cast[l] != undefined) {
+            //         attori += cast[l].name + " ";
+            //       };
+            //     };
+            //     // console.log( "questo ........................", attori);
+            //   }
+            // });
+            // // console.log( "questo ........................", attori);
+          caricaAttori(tipo,risultati,i,listaAttori);
+
           var immagineUrl = '<img src="https://image.tmdb.org/t/p/w342'+ risultati[i].poster_path + '">';
-          console.log(risultati);
-          console.log(risultati[i].poster_path);
-          var risultato = {
+            risultato = {
             titolo: risultati[i][titolo],
             titoloOr: risultati[i][titoloOriginale],
             lingua: bandiere(risultati[i].original_language),
             voto: votoStelle(risultati[i].vote_average),
-            immagine: immagineUrl
+            descrizione: risultati[i].overview,
+            tipo: tipo,
+            immagine: immagineUrl,
+            // attori: attori
+            // genere: genere
           }
+
           $(".risultatiRicerca").append(template(risultato))
         }
+
+        $(".bo").click(function(){
+
+          var indice = $(this).parent().index();
+          $(this).siblings(".finalmenteAttori").html(listaAttori[indice])
+        })
       },
       error: function(richiesta,stato,errore){
         alert("Chiamata fallita!!!");
@@ -52,7 +92,7 @@ $(document).ready(function () {
 
   function votoStelle(votoNumerico){
 
-    var voto = Math.floor(votoNumerico/2);
+    var voto = Math.ceil(votoNumerico/2);
     var stelle= "";
     for (var j = 0; j < voto; j++) {
       stelle += '<i class="fa fa-star" aria-hidden="true"></i>';
@@ -66,18 +106,42 @@ $(document).ready(function () {
   //crea bandiere
 
   function bandiere(lingua){
-    if (lingua == "it") {
-      lingua = '<img src="immagini/it.png" alt="">';
-    }else if (lingua == "en") {
-      lingua = '<img src="immagini/gb.png" alt="">';
-    }else if (lingua == "es") {
-      lingua = '<img src="immagini/es.png" alt="">';
-    }else if (lingua == "de") {
-      lingua = '<img src="immagini/de.png" alt="">';
-    }else {
-      lingua = lingua;
+    var lingue= ["it", "en","es","de"];
+    if (lingue.includes(lingua)) {
+      lingua = '<img src="immagini/' + lingua + '.png">'
     }
     return lingua;
   };
+
+
+
+  // attori
+  function caricaAttori(tipo,risultati,i,array){
+    var urlAttori;
+    if (tipo == "serie") {
+      urlAttori = 'https://api.themoviedb.org/3/tv/'+ risultati[i].id +'/credits'
+    }else if (tipo == "film") {
+      urlAttori = 'https://api.themoviedb.org/3/movie/'+ risultati[i].id +'/credits'
+    }
+    $.ajax({
+      url: urlAttori,
+      data: {
+        api_key: "337a5096c47a89ed7fe0ca372a05c5bc",
+      },
+      method: "GET",
+      success: function (data,stato) {
+        console.log("i della success", i);
+        var cast = data.cast;
+        var attori="";
+        for (var l = 0; l < 5; l++) {
+          if (cast[l] != undefined) {
+            attori += cast[l].name + " "
+          };
+        };
+        array[i] = attori;
+      }
+    });
+  };
+
 
 });
